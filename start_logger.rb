50b05ef0dff68dc20ffa40419fa0f4f872b1ec5b
CryptoRireal <crypto@rireal.com>
2018-01-28 16:11:40 -0500

    Use an example config file by default
    This forces the user to explicitly create it

--- start_logger.rb
+++ start_logger.rb
@@ -1,5 +1,9 @@
-require "drb/drb"
+unless File.exist?(File.join(File.dirname(__FILE__), "./app.config"))
+  puts "Couldn't find app.config! Make sure the file exists."
+  return
+end
 
+require "drb/drb"
 require_relative "lib/profit_trailer"
 
 puts "Connecting to ProfitTrailer Slack bot..."
